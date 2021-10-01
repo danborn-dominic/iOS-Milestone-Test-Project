506053fd209e807c7aec7404d0f4b426f2283aa2
danborn-dominic <ddanborn@purdue.edu>
2021-10-01 15:04:40 -0400

    Lesson 3, made social squid again but using StackView and fewer autolayout constraints.

---
+++ Lesson2StackView/Lesson2StackView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson2StackView
+//
+//  Created by Dominic Danborn on 10/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
